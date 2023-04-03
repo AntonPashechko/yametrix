69221234f589260100905740b7dc8ef3bd32d2d2
Anton Pashechko <pashechko_a@gaz-is.ru>
2023-04-03 23:20:29 +0300

    Переписал роутинг, оставил только update, остальное через сплит получаю

--- internal/handlers/metrix/handler.go
+++ internal/handlers/metrix/handler.go
@@ -9,82 +9,56 @@ import (
 )
 
 const (
-	updateGaugeURL    = "/update/gauge/"
-	updateCounterURL  = "/update/counter/"
-	notImplementedURL = "/"
+	updateURL = "/update/"
 )
-
-func getKeyValue(uri string, prefix string) (bool, []string) {
-	data := strings.TrimPrefix(uri, prefix)
-	if data == "" {
-		return false, nil
-	}
-
-	keyValue := strings.Split(data, "/")
-
-	if len(keyValue) != 2 {
-		return false, nil
-	}
-
-	return true, keyValue
-}
 
 type Handler struct {
 	Storage storage.MertixStorage
 }
 
 func (h *Handler) Register(router *http.ServeMux) {
-	router.HandleFunc(updateGaugeURL, h.updateGauge)
-	router.HandleFunc(updateCounterURL, h.updateCounter)
-	router.HandleFunc(notImplementedURL, h.notImplemented)
+	router.HandleFunc(updateURL, h.update)
 }
 
-func (h *Handler) updateGauge(w http.ResponseWriter, r *http.Request) {
+func (h *Handler) update(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		w.WriteHeader(http.StatusMethodNotAllowed)
 		return
 	}
 
-	ok, keyValue := getKeyValue(r.RequestURI, updateGaugeURL)
-	if !ok {
+	data := strings.TrimPrefix(r.RequestURI, updateURL)
+	if data == "" {
 		w.WriteHeader(http.StatusNotFound)
 		return
 	}
 
-	value, err := utils.StrToFloat64(keyValue[1])
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-
-	h.Storage.Set(keyValue[0], value)
-
-	w.WriteHeader(http.StatusOK)
-}
-
-func (h *Handler) updateCounter(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		w.WriteHeader(http.StatusMethodNotAllowed)
-		return
-	}
-
-	ok, keyValue := getKeyValue(r.RequestURI, updateCounterURL)
-	if !ok {
+	datas := strings.Split(data, "/")
+	if len(datas) != 3 {
 		w.WriteHeader(http.StatusNotFound)
 		return
 	}
 
-	value, err := utils.StrToInt64(keyValue[1])
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
+	switch datas[0] {
+	case "gauge":
+		value, err := utils.StrToFloat64(datas[2])
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		h.Storage.Set(datas[0], value)
+	case "counter":
+		value, err := utils.StrToInt64(datas[2])
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		h.Storage.Add(datas[0], value)
+	default:
+		w.WriteHeader(http.StatusNotImplemented)
 		return
 	}
 
-	h.Storage.Add(keyValue[0], value)
-
 	w.WriteHeader(http.StatusOK)
-}
-
-func (h *Handler) notImplemented(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusNotImplemented)
 }
