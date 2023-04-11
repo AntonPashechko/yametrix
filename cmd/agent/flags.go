47e47581169c51c15e1da1873eb6467e881bf4b0
Anton Pashechko <pashechko_a@gaz-is.ru>
2023-04-12 00:16:10 +0300

    Перезапуск тестов

--- cmd/agent/flags.go
+++ cmd/agent/flags.go
@@ -16,5 +16,6 @@ func parseFlags() {
 	flag.StringVar(&options.serverEndpoint, "a", "http://localhost:8080", "server address and port")
 	flag.Int64Var(&options.reportInterval, "r", 10, "report interval")
 	flag.Int64Var(&options.pollInterval, "p", 2, "poll interval")
+
 	flag.Parse()
 }
