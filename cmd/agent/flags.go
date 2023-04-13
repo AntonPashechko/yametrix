64cdb207e0bc7647465e62f5707f29cb694653eb
Anton Pashechko <pashechko_a@gaz-is.ru>
2023-04-13 12:22:19 +0300

    Итерация 5, разбор переменных окружения

--- cmd/agent/flags.go
+++ cmd/agent/flags.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"flag"
+	"os"
 	"strings"
+
+	"github.com/AntonPashechko/yametrix/pkg/utils"
 )
 
 type Options struct {
@@ -14,11 +17,29 @@ type Options struct {
 var options Options
 
 func parseFlags() {
+	/*Получаем параметры из командной строки*/
 	flag.StringVar(&options.serverEndpoint, "a", "http://localhost:8080", "server address and port")
 	flag.Int64Var(&options.reportInterval, "r", 10, "report interval")
 	flag.Int64Var(&options.pollInterval, "p", 2, "poll interval")
 
 	flag.Parse()
+
+	/*Но если заданы в окружении - берем оттуда*/
+	if addr, exist := os.LookupEnv("ADDRESS"); exist {
+		options.serverEndpoint = addr
+	}
+
+	if interval, exist := os.LookupEnv("REPORT_INTERVAL"); exist {
+		if val, err := utils.StrToInt64(interval); err == nil {
+			options.reportInterval = val
+		}
+	}
+
+	if interval, exist := os.LookupEnv("POLL_INTERVAL"); exist {
+		if val, err := utils.StrToInt64(interval); err == nil {
+			options.pollInterval = val
+		}
+	}
 
 	if !strings.HasPrefix(options.serverEndpoint, "http") && !strings.HasPrefix(options.serverEndpoint, "https") {
 		options.serverEndpoint = "http://" + options.serverEndpoint
--- cmd/server/flags.go
+++ cmd/server/flags.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"os"
 )
 
 var endpoint string
@@ -9,4 +10,9 @@ var endpoint string
 func parseFlags() {
 	flag.StringVar(&endpoint, "a", "localhost:8080", "address and port to run server")
 	flag.Parse()
+
+	/*Но если заданы в окружении - берем оттуда*/
+	if addr, exist := os.LookupEnv("ADDRESS"); exist {
+		endpoint = addr
+	}
 }
